f30a66d146dbb0174658c95cc77cef1390b9fb05
Ali Njie <apfnjie@gmail.com>
2015-11-07 17:32:46 +0000

    Backend: Controller implements post

--- controller/controller.php
+++ controller/controller.php
@@ -1,88 +1,161 @@
 <?php
+// Recieving command
   $cmd = $_REQUEST['cmd'];
 
+// Switch statement to handle different type of commands
   switch ($cmd) {
+
+    // case 1 add a post
     case 1:
-      add_post($user_id,$content,$category);
+      add_post($_REQUEST['user'],$_REQUEST['content'],$_REQUEST['category']);
       break;
+
+    // case 2 get post from a particular user
     case 2:
-      get_user_post($user_id);
+      get_user_post($_REQUEST['user']);
       break;
+
+    // Get all post
     case 3:
       get_all_post();
       break;
+
+    // Get a given amount of recent post
     case 4:
+      $num = 10;
       get_recent_post($num);
       break;
+
+    // Get all post from a given category
     case 5:
       get_post_by_category($category);
       break;
+
     default:
-      echo '{"cmd":"none","message":"No command issued"}';
       break;
   }
 
+/*
+ * function to convert sql data into json format
+ */
 
-  function add_post($user_id,$content,$category){
-    include_once("post.php");
-    $post = new Post();
-
-    if(!$post->add_post($user_id,$content,$category)){
-      echo '{"result":0,"message":"Could not add the post"}';
-      return;
-    }
-
-    echo '{"result":1,"message":"Succesfully added the post"}';
+/*
+ * function add a post
+ */
+function add_post($user_id,$content,$category){
+  include_once("post.php");
+  $post = new Post();
+  // if post failed give error message
+  if(!$post->add_post($user_id,$content,$category)){
+    echo '{"result":0,"message":"Post not completed, try again"}';
+    return;
+  }
+  // give feedback
+  echo '{"result":1,"message":"Post successfully created"}';
   }
 
-  function get_user_post($user_id){
-    include_once("post.php");
-    $post = new Post();
+/*
+ * function to get post from a given user
+ */
+function get_user_post($user_id){
+  include_once("post.php");
+  $post = new Post();
+  $post->get_user_post($user_id);
+  $row = $post->fetch();
 
-    if(!$post->get_user_post($user_id)){
+  if(!$row) {
+    echo '{"result":0,"message":"Could not get post from the user"}';
+    return;
+  }
+  // send data in json format
+  echo '{"result":1,"post["';
+  while($row) {
+    echo json_encode($row);
+    $row = $post->fetch();
+    if($row) {
+      echo ',';
+    }
+  }
+  echo ']}';
+  return;
+  }
+
+/*
+ * function to get all post
+ */
+function get_all_post(){
+  include_once("post.php");
+  $post = new Post();
+  $post->get_all_post();
+  $row = $post->fetch();
+
+    if(!$row) {
       echo '{"result":0,"message":"Could not get post from the user"}';
       return;
     }
-
-    echo '{"result":1,"message":"Succesfully got the post"}';
-  }
-
-  function get_all_post(){
-    include_once("post.php");
-    $post = new Post();
-
-    if(!$post->get_user_post()){
-      echo '{"result":0,"message":"Could not get all the post"}';
-      return;
+    // send data in json format
+    echo '{"result":1,"post["';
+    while($row) {
+      echo json_encode($row);
+      $row = $post->fetch();
+      if($row) {
+        echo ',';
+      }
     }
-
-    echo '{"result":1,"message":"Succesfully got all posts"}';
+    echo ']}';
+    return;
   }
 
-  function get_recent_post($num){
-    include_once("post.php");
-    $post = new Post();
+/*
+ * function to get the recent number of post
+*/
+function get_recent_post($num){
+  include_once("post.php");
+  $post = new Post();
+  $post->get_recent_post($num);
+  $row = $post->fetch();
 
-    if(!$post->get_user_post($num)){
-      echo '{"result":0,"message":"Could not get the recent posts"}';
-      return;
+  if(!$row) {
+    echo '{"result":0,"message":"Could not get post from the user"}';
+    return;
+  }
+  // send data in json format
+  echo '{"result":1,"post["';
+  while($row) {
+    echo json_encode($row);
+    $row = $post->fetch();
+    if($row) {
+      echo ',';
     }
-
-    echo '{"result":1,"message":"Succesfully got the recent posts"}';
+  }
+  echo ']}';
+  return;
   }
 
-  function get_post_by_category($category){
-    include_once("post.php");
-    $post = new Post();
+/*
+ * function to get post from certain category
+ */
+function get_post_by_category($category){
+  include_once("post.php");
+  $post = new Post();
+  $post->get_post_by_categoryt($category);
+  $row = $post->fetch();
 
-    if(!$post->get_user_post($category)){
-      echo '{"result":0,"message":"Could not get the posts by category"}';
-      return;
+  if(!$row) {
+    echo '{"result":0,"message":"Could not get post from the user"}';
+    return;
+  }
+  // send data in json format
+  echo '{"result":1,"post["';
+  while($row) {
+    echo json_encode($row);
+    $row = $post->fetch();
+    if($row) {
+      echo ',';
     }
-
-    echo '{"result":1,"message":"Succesfully got posts by category"}';
   }
+  echo ']}';
+  return;
+}
 
-
-  // function
  ?>
